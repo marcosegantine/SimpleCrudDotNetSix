69e4395ed39b51058c7b388000c35f686e34cbe9
Marcos Segantine <marcosegantine@hotmail.com>
2023-07-06 22:27:26 +0100

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleCrudDotNetSix.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SimpleCrudDotNetSix.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private static List<User> Users()
+        {
+            return new List<User>
+            {
+                new User{Name = "Marcos", Id = 1, DateOfBirth = new DateTime(1987,02,15)}
+            };
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Users());
+        }
+
+        [HttpPost]
+        public IActionResult Post(User user)
+        {
+            var users = Users();
+            users.Add(user);
+
+            if (user == null)
+            {
+                return BadRequest("Insira um nome válido");
+            }
+            return Ok(users);
+
+        }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleCrudDotNetSix.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
